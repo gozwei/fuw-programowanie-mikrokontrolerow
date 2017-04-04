702896d0300f16ff9e1e96d88eb1e451f4cc9bb3
gozwei <marcin@marcinpolkowski.com>
2017-04-04 13:50:49 +0200

    Modyfikacja CW05

--- CW05/TC77_spi_hard/tc77_hard.bat
+++ CW05/TC77_spi_hard/tc77_hard.bat
@@ -1,6 +1,6 @@
 avr-g++ -mmcu=atmega32 -DF_CPU=16000000 -Wall -O2 -gstabs -ffunction-sections -fdata-sections -c tc77_hard.cc
 avr-g++ -mmcu=atmega32 -DF_CPU=16000000 -Wall -O2 -gstabs -ffunction-sections -fdata-sections -c biblio.cc
-avr-g++ -mmcu=atmega32 -o tc77_hard tc77_hard.o biblio.o
+avr-g++ -mmcu=atmega32 -Wl,-u,vfprintf -lprintf_flt -lm -o tc77_hard tc77_hard.o biblio.o
 avr-objcopy -O ihex tc77_hard tc77_hard.hex
 avr-size -C --mcu=atmega32 tc77_hard
 kamprogavrc -d -f tc77_hard.hex
